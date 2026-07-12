9245de22d9405ab85f2293160804545cdb0b6455
Thiago Rodrigues <thiagorodrigues1203@outlook.com>
2026-07-12 20:59:16 -0300

    CONFIG FOR DEPLOY

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,14 @@
 //     React/TanStack dedupe, error logger plugins, and sandbox detection (port/host/strictPort).
 // You can pass additional config via defineConfig({ vite: { ... }, etc... }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   tanstackStart: {
     // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
     // nitro/vite builds from this
     server: { entry: "server" },
+    plugins: [react()],
+    base: 'https://github.com/thi-rodrigues/word-master-game', // Preencha aqui
   },
 });
