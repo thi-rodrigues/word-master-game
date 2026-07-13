b4112eed95ecb419c3c3d44923b86fe8fc913f55
Thiago Rodrigues <thiagorodrigues1203@outlook.com>
2026-07-12 21:45:12 -0300

    Revert "ajustes"
    This reverts commit 30102afa5da710bde76be0c818392100b5a89d27.

--- nitro.config.ts
+++
@@ -1,4 +0,0 @@
-export default {
-  base: '/word-master-game/',
-  preset: 'vercel-static'
-};
--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,15 @@
 //     nitro (build-only using cloudflare as a default target), VITE_* env injection, @ path alias,
 //     React/TanStack dedupe, error logger plugins, and sandbox detection (port/host/strictPort).
 // You can pass additional config via defineConfig({ vite: { ... }, etc... }) if needed.
-// import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-import { defineConfig } from 'vite'
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // tanstackStart: {
+  tanstackStart: {
     // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
     // nitro/vite builds from this
-    // server: { entry: "server" },
-    plugins: [react(),  ],
-    base: '/word-master-game/',
-  // },
+    server: { entry: "server" },
+    plugins: [react()],
+    base: 'https://github.com/thi-rodrigues/word-master-game', // Preencha aqui
+  },
 });
