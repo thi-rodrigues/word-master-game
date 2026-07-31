8818cb584085657d5845628f7359b0427fcd92c4
Thiago Rodrigues <thiagorodrigues1203@outlook.com>
2026-07-31 10:07:59 -0300

    correção

--- server.js
+++ server.js
@@ -6,17 +6,17 @@ const port = Number(process.env.PORT || 3000);
 const wordsFile = path.join(__dirname, 'public', 'words.json');
 
 const express = require("express");
-const path = require("path");
+// const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "dist/word-master-game/browser")));
+// app.use(express.static(path.join(__dirname, "dist/word-master-game/browser")));
 
-app.get("*", (req, res) => {
-    res.sendFile(
-        path.join(__dirname, "dist/word-master-game/browser/index.html")
-    );
-});
+// app.get("*", (req, res) => {
+//     res.sendFile(
+//         path.join(__dirname, "dist/word-master-game/browser/index.html")
+//     );
+// });
 
 function send(response, status, body) {
   response.writeHead(status, { 'Content-Type': 'application/json; charset=utf-8' });
